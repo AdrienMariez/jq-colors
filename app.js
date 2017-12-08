f6cca11d0efc3330f14aa065197d50993028993c
Adrien Mariez <adrien.mariez@gmail.com>
2017-12-08 12:23:09 +0100

    commentaires pour la correction

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ $(document).ready(function(){
 
 	var couleur = 0 ;
 
-		$('.color').each(function(){
-			var couleur = $(this).attr("data-color");
-			$(this).css("background", couleur);		
+		$('.color').each(function(){							//quand je charge la page, pour tous les boutons
+			var couleur = $(this).attr("data-color");			//je récupère la valeur de l'attribut data-color de chaque bouton indépendamment
+			$(this).css("background", couleur);					//ça change la valeur de fond de ce même bouton
 		});
 
-		$('.color').click(function(){
-			var couleur = $(this).attr("data-color");
+		$('.color').click(function(){							//je clique sur un .button
+			var couleur = $(this).attr("data-color");			//je récupère la valeur de l'attribut data-color
 
-			if ( $('input[name="cocoroco"]').is(':checked')) {
-  				$(".main").css('color', couleur);
+			if ( $('input[name="cocoroco"]').is(':checked')) {	//si cocorico est coché
+  				$(".main").css('color', couleur);				//change la couleur du texte de .main
   			}
 
   			else{
-				$('.main').css('backgroundColor', couleur);
+				$('.main').css('backgroundColor', couleur);		//sinon ça change la couleur de fond de .main
   			}
   	});
 });
