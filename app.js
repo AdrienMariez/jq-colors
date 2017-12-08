34055303491f191d98f75b0b05d0c7ac4ab33344
Adrien Mariez <adrien.mariez@gmail.com>
2017-12-08 12:12:01 +0100

    exercice resolu

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// good luck !
+$(document).ready(function(){
+
+	var couleur = 0 ;
+
+		$('.color').each(function(){
+			var couleur = $(this).attr("data-color");
+			$(this).css("background", couleur);		
+		});
+
+		$('.color').click(function(){
+			var couleur = $(this).attr("data-color");
+
+			if ( $('input[name="cocoroco"]').is(':checked')) {
+  				$(".main").css('color', couleur);
+  			}
+
+  			else{
+				$('.main').css('backgroundColor', couleur);
+  			}
+  	});
+});
